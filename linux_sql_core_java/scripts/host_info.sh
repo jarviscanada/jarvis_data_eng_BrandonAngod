ae7f21b05686c06434eef483120fe81b6de25454
Brandon Angod <brandon.angod@ontariotechu.net>
2021-09-29 23:06:54 +0000

    Added PSQL output to Usage and Info

--- linux_sql_core_java/scripts/host_info.sh
+++ linux_sql_core_java/scripts/host_info.sh
@@ -2,6 +2,8 @@
 hostname=$(hostname -f)
 
 #save number of CPU to a variable
+export PGPASSWORD='password'
+
 lscpu_out=`lscpu`
 #note: `xargs` is a trick to remove leading and trailing white spaces
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
@@ -15,5 +17,13 @@ cpu_mhz=$(echo "$lscpu_out"  | egrep '^CPU[[:space:]]MHz:' | awk '{print $3}' | 
 l2_cache=$(echo "$lscpu_out"  | egrep 'L2[[:space:]]cache:' | awk '{print $3}' | xargs)
 
 meminfo_out=`cat /proc/meminfo`
-total_mem=total_mem=$(echo "$meminfo_out"  | egrep '^MemTotal\:' | awk '{print $2}' | xargs)
-timestamp=date "+%Y/%m/%d %H:%M:%S" #current timestamp in `2019-11-26 14:40:19` format
+total_mem=$(echo "$meminfo_out"  | egrep '^MemTotal\:' | awk '{print $2}' | xargs)
+timestamp=$(date '+%Y-%m-%d %H:%M:%S') #current timestamp in `2019-11-26 14:40:19` format
+
+psql -h localhost -U postgres -w << EOF
+\dt
+\c host_agent
+\dt
+INSERT INTO host_info VALUES ('1','$hostname','$cpu_number','$cpu_architecture','$cpu_model','$cpu_mhz','$l2_cache','$total_mem','$timestamp');
+SELECT *FROM host_info
+EOF
--- linux_sql_core_java/scripts/host_usage.sh
+++ linux_sql_core_java/scripts/host_usage.sh
@@ -1,13 +1,30 @@
-
+export PGPASSWORD='password'
 #save hostname to a variable
 hostname=$(hostname -f)
 
 meminfo_out=`cat /proc/meminfo`
 #usage
-memory_free=memory_free=$(echo "$meminfo_out"  | egrep '^MemFree:' | awk '{print $2}' | xargs)
+timestamp=$(date '+%Y-%m-%d %H:%M:%S') #current timestamp in `2019-11-26 14:40:19` format
+memory_free=$(echo "$meminfo_out"  | egrep '^MemFree:' | awk '{print $2}' | xargs)
 #NR= row number
 #NF= Field number
 cpu_idle=$(vmstat | awk 'NR==3 {print $(NF-2)}')
 cpu_kernel=$(vmstat | awk 'NR==3 {print $(NF-1)}')
 disk_io=$(vmstat -d | awk 'NR==3 {print $(NF-1)}')
 disk_available=$(df -BM / | awk 'NR==2 {print $(NF-2)}')
+
+echo $memory_free
+echo $cpu_idle
+echo $cpu_kernel
+echo disk_io
+echo disk_available
+
+psql -h localhost -U postgres -w << EOF
+\dt
+\c host_agent
+\dt
+INSERT INTO host_usage VALUES ('$timestamp','1','$memory_free','$cpu_idle','$cpu_kernel','$disk_io','$disk_available');
+SELECT *FROM host_usage
+EOF
+
+#psql -h localhost -U postgres -d postgres -W -c "INSERT INTO PUBLIC.host_info ($meminfo,$memory_free,$cpu_idle,$cpu_kernel,$disk_io,$disk_available)"
