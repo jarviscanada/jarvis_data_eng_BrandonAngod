61b39b9a2c3bf5c707143322462ff055b30e336c
Brandon Angod <brandon.angod@ontariotechu.net>
2021-10-12 19:27:08 +0000

    Added Grepapp 's souce code

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/grepapp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/grepapp.java
@@ -1,9 +1,44 @@
 package ca.jrvs.apps.practice;
+import java.io.*;
+import java.util.Scanner;
+
+
 public class grepapp {
-    public static void main(String args[]) {
+    private static String ConcatFile(File file) throws FileNotFoundException {
+        String concat="";
+        Scanner sc1= new Scanner(file);
+        while(sc1.hasNextLine()){
+            concat=concat+sc1;
+        }
+        sc1.close();
+        return(concat);
+    }
+    
+    private static void StringRegexParser(){
+
+    }
+    public static void main(String args[]) throws IOException  {
+        //initialize variables
         String regexPattern=args[0];
         String rootPath=args[1];
         String outFile=args[2];
-        //System.out.println("Yessir");
+        FileWriter fwrite=new FileWriter(outFile);
+
+
+        //Convert to File type
+        File path = new File(rootPath);
+        File fileArray[] = path.listFiles();
+
+
+        //Iterate through files and Match the pattern
+        for(File file : fileArray){
+            //Easier route using regex patterns
+            String fileContents = ConcatFile(file);
+            if(fileContents.matches(regexPattern)){
+                fwrite.write(file.getName() + "\n");
+            }
+        }
+        //Cleanup
+        fwrite.close();
     }
 }
